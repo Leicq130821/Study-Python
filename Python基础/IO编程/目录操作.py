c06cbcc8ed3da64811622a6d20727b5ae0160603
Leicq130821 <583870243@qq.com>
2020-03-31 22:30:59 +0800

    提交目录操作与学习日志

--- Python基础/IO编程/目录操作.py
+++ Python基础/IO编程/目录操作.py
@@ -1,11 +1,22 @@
 import os
-#getcwd()函数：获取当前目录的绝对路径。
+#getcwd()：获取当前目录的绝对路径。
 print(os.getcwd())
-# mkdir(path)函数：创建一级目录，需要保证path里父级目录必须存在且创建的目录不存在,否则将会报错。
-# rmdir(path)函数：删除一级目录，需要保证目录必须存在且为空目录，否则将会报错。
-os.mkdir(r'D:\mkdir\dir')
-os.rmdir(r'D:\mkdir\dir')
-# makedirs(path)函数：创建多级目录。
-# removedirs(path)函数：删除多级目录。
-os.makedirs(r'D:\makedirs\dirs\dir')
-os.removedirs(r'D:\makedirs\dirs\dir')
+# mkdir(path)：创建单级目录，需要保证path里父级目录必须存在且创建的目录不存在,否则将会报错。
+# rmdir(path)：删除单级目录，需要保证目录必须存在且为空目录，否则将会报错。
+os.mkdir(r'D:\Python\mkdir\dir')
+os.rmdir(r'D:\Python\mkdir\dir')
+# makedirs(path)：如果path里父级目录不存在，则会一起创建，需要保证创建的最底层目录不存在，否则将会报错。
+# removedirs(path)：如果path里有多级目录，则会删除多级目录，顺序是先删除子目录，然后再删除父目录，且目录为空时才能删除。
+os.makedirs(r'D:\Python\makedirs\dirs')
+os.removedirs(r'D:\Python\makedirs\dirs')
+# chdir(path)：改变当前文件目录
+# os.rename(oldpath,newpath)：重命名文件或者路径。
+os.rename(r'D:\Python\TEST\test.txt',r'D:\Python\TEST\TEST.txt')
+# os.path.join(path,filename)：目录与文件名称合成绝对路径。
+print(os.path.join(r'D:\TEST','TEST.txt'))
+# os.path.split(path)：拆分路径信息，返回两个元素的元组，后面一个元素为末级目录或者是文件名。
+print(os.path.split(r'D:\Python\mkdir'))
+print(os.path.split(r'D:\Python\TEST\TEST.txt'))
+# os.path.splitext(path)：拆分路径信息，返回两个元素的元组，后面一个元素为文件扩展名。
+print(os.path.splitext(r'D:\Python\mkdir'))
+print(os.path.splitext(r'D:\Python\TEST\TEST.txt'))
