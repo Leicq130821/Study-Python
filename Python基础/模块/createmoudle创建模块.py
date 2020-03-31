99690d11a7e3b9d8f84334765acb94b9dbc48e22
Leicq130821 <583870243@qq.com>
2020-03-31 09:11:51 +0800

    格式化

--- Python基础/模块/createmoudle创建模块.py
+++ Python基础/模块/createmoudle创建模块.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 # 创建模块其实就是建一个.py文件，通常情况下会把类似的功能代码写在同一个.py文件中
+# -*- coding: utf-8 -*-
 class Moudle():
     def func1(x,y):
-        print(11111)
-        print(__name__)
         return x*y-(x+y)
     def func2(x,y):
-        print(22222)
-        print(__name__)
         return pow(x,y)-(x+y)
 if __name__ == '__main__':
     print(Moudle.func1(4,2))
