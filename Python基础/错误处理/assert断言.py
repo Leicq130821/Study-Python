451d03aa4d97793f471706007e7f4b038d810501
Leicq130821 <583870243@qq.com>
2020-03-31 16:20:34 +0800

    格式化

--- Python基础/错误处理/assert断言.py
+++ Python基础/错误处理/assert断言.py
@@ -1,14 +1,14 @@
 # 可以通过assert断言的方式来检查错误，assert的基本语法为：assert 表达式,"断言语句"。
-# 如果表达式返回的结果是False时，则抛出AssertionError并抛出断言语句。
-age=int(input('请输入年龄：'))
-assert 100>age>2,'年龄必须大于2小于100'
-if age<7:
-    print('你的年龄是儿童')
-elif age<18:
-    print('你的年龄是少年')
-elif age<45:
-    print('你的年龄是青年')
-elif age<60:
-    print('你的年龄是中年')
-else:
-    print('你的年龄是老年')
+# 如果表达式返回的结果是False时，则抛出AssertionError并抛出断言语句，断言异常可以被except捕获。
+def Diomod(x,y):
+    try:
+        assert isinstance(x,(int,float)) and isinstance(y,(int,float)),'被除数与除数类型不正确'
+        assert y!=0,'除数不能为0'
+    except Exception as E:
+        print('输入错误：',E)
+    else:
+        print('输入正确，结果为：',x/y)
+if __name__ == '__main__':
+    Diomod(10,0)
+    Diomod('a',1)
+    Diomod(10,2)
