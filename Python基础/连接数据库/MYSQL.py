603b5e819f86d7e8198dc346b542fa094f265822
Leicq130821 <583870243@qq.com>
2020-04-14 09:39:28 +0800

    提交MYSQL

--- Python基础/连接数据库/MYSQL.py
+++ Python基础/连接数据库/MYSQL.py
@@ -5,26 +5,22 @@ MYSQL=pymysql.connect('127.0.0.1','root','000000','TEST',3306)
 # 创建索引
 Cursor=MYSQL.cursor()
 # 执行语句
-try:
-    Cursor.execute('''
-        create table if not exists Student(
-            Serial_no int not null auto_increment primary key,
-            Name varchar(20),
-            Age int,
-            Address varchar(30)
-            )
+Cursor.execute('''
+    create table if not exists Student(
+        Serial_no int not null auto_increment primary key,
+        Name varchar(20),
+        Age int,
+        Address varchar(30)
+        )
 ''')
-    Cursor.execute('''
-        insert into Student(Name,Age,Address)
-        values('小明',20,'北京市朝阳区'),
-              ('小红',21,'上海市徐汇区'),
-              ('小刚',22,'杭州市余杭区')
+Cursor.execute('''
+    insert into Student(Name,Age,Address)
+    values('小明',20,'北京市朝阳区'),
+          ('小红',21,'上海市徐汇区'),
+           ('小刚',22,'杭州市余杭区')
 ''')
-    # 提交事务
-    MYSQL.commit()
-except Exception as E:
-    print(E)
-    MYSQL.rollback()
+# 提交事务
+MYSQL.commit()
 # 查询信息
 Cursor.execute('select * from Student')
 student=Cursor.fetchall()
